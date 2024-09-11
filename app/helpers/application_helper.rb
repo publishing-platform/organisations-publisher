9befb0bd41f8e10af28adb21dd514323aaa01bf7
neilha <neilharrold@gmail.com>
2024-09-11 17:47:49 +0100

    basic set up of app styles/js and organisations index route

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def navigation_items
+    return [] unless current_user
+
+    items = []
+
+    items << { text: current_user.name, href: PublishingPlatformLocation.external_url_for("signon") }
+    items << { text: "Sign out", href: publishing_platform_sign_out_path }
+  end
+
+  def is_current?(link)
+    recognized = Rails.application.routes.recognize_path(link)
+    recognized[:controller] == params[:controller] &&
+      recognized[:action] == params[:action]
+  end  
 end
