3a62000927b2af4ce054cc642fa86ec445a2ee5b
neilha <neilharrold@gmail.com>
2025-03-26 19:02:29 +0000

    rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get "/healthcheck/live", to: proc { [200, {}, %w[OK]] }
-  
+
   root to: redirect("/organisations")
 
   resources :organisations, except: %i[show destroy]
